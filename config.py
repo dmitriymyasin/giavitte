edd50b82f67e8c38906e2f36f9ffe288f5df7115
dmitriymyasin <59253870+dmitriymyasin@users.noreply.github.com>
2025-12-19 10:38:22 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,16 @@
 import os
 from dotenv import load_dotenv
+import sys
 
 load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'dev-secret-key')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', 'mysql+pymysql://root:HfgFGty217GF@localhost/vitte')
+    
+    # Используем SQLite вместо MySQL
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI', f'sqlite:///{os.path.join(basedir, "vitte.db")}')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     ADMIN_LOGIN = os.getenv('ADMIN_LOGIN', 'Admin')
     ADMIN_PASSWORD = os.getenv('ADMIN_PASSWORD', 'KorokNET')
